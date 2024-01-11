e283e4ed6932379ace0ba5d0b629cb3c72b3e50a
mac <mac@macs-MacBook-Pro.local>
2024-01-11 21:08:22 +0100

    implemented the update feature

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ class Collection:
         self.__records.append({self.counter: record})
         self.counter += 1
 
-    def results(self):
-        return self.__records
-    
-    def update(self, key, value)
+    def update(self, key, value):
         for entry in self.__records:
             entry[key] = value
 
+    def results(self):
+        return self.__records
 
     def __repr__(self) -> str:
         for record in self.__records:
@@ -35,17 +34,13 @@ class KeyValueStore:
         c.add(data)
         self.collections.append(c)
 
-
     def update(self, collection: str, key, value) -> None:
         for item in self.collections:
             if item.name == collection:
                 item.update(key, value)
 
-
-
     def display(self, collection: str) -> None:
         for item in self.collections:
-            
             if item.name == collection:
                 print(item.results)
 
@@ -54,4 +49,4 @@ db = KeyValueStore()
 db.insert("users", {"name": "Dapo", "state_of_origin": "Lagos", "gender": "Male"})
 db.insert("users", {"name": "Taiwo", "state_of_origin": "Ondo", "gender": "Female"})
 db.display("users")
-db.update("users", {"name": "Taiwo", "state_of_origin": "Ondo", "gender": "Female"})
+# db.update("users", {"name": "Taiwo", "state_of_origin": "Ondo", "gender": "Female"})
